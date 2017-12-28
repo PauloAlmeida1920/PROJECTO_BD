65796bf17489d5a9e2489239a24888067014f5da
Paulo Almeida <paulo.cruz1920@gmail.com>
2017-12-28 12:08:01 +0000

    alteração codificação

---
+++ charaterSet_utf8.sql
@@ -0,0 +1,23 @@
+ALTER TABLE `Cliente` 
+CHARACTER SET = utf8 , COLLATE = utf8_unicode_ci ;
+
+ALTER TABLE `Empregados` 
+CHARACTER SET = utf8 , COLLATE = utf8_unicode_ci ;
+
+ALTER TABLE `Encomendas` 
+CHARACTER SET = utf8 , COLLATE = utf8_unicode_ci ;
+
+ALTER TABLE `Fornecedores` 
+CHARACTER SET = utf8 , COLLATE = utf8_unicode_ci ;
+
+ALTER TABLE `LocalTrabalho` 
+CHARACTER SET = utf8 , COLLATE = utf8_unicode_ci ;
+
+ALTER TABLE `Pagamento` 
+CHARACTER SET = utf8 , COLLATE = utf8_unicode_ci ;
+
+ALTER TABLE `Pedido` 
+CHARACTER SET = utf8 , COLLATE = utf8_unicode_ci ;
+
+ALTER TABLE `Restaurantes` 
+CHARACTER SET = utf8 , COLLATE = utf8_unicode_ci ;
